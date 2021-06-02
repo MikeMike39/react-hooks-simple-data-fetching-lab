4e25c94bc48b4b4e91324b3cb1657b2f944ed3d6
MikeMike39 <Mfonseca294@gmail.com>
2021-06-02 13:37:11 -0400

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [peopleInSpace, setPeopleInSpace] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setPeopleInSpace(data.people);
+      });
+  }, []);
+ 
+
+  return <div>{peopleInSpace.map((person) => person.name)}</div>;
+}
+
+export default App; 
